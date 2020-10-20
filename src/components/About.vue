5e4096bddbf02f1ae2e2d7c635c9f910d6db5636
MyKarken <beast964089@gmail.com>
2020-10-20 15:08:03 +0800

    版面完成

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,37 +1,115 @@
 <template>
   <div class="box">
     <div class="page item-1">
-      <div class="page-header">header</div>
-      <div class="page-body">body</div>
+      <div class="page-header">OUR HISTORY-1998</div>
+      <div class="page-body">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
     </div>
     <div class="page item-2">
-      <div class="page-header">header</div>
-      <div class="page-body">body</div>
+      <div class="page-header">OPPORTUNITIES</div>
+      <div class="page-body">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
     </div>
     <div class="page item-3">
-      <div class="page-header">header</div>
-      <div class="page-body">body</div>
+      <div class="page-header">OPPORTUNITIES</div>
+      <div class="page-body">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
     </div>
     <div class="page item-4">
-      <div class="page-header">header</div>
-      <div class="page-body">body</div>
-      <div class="page-footer"></div>
+      <div class="page-header">OUR HISTORY-2001</div>
+      <div class="page-body">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
     </div>
     <div class="page item-5">
-      <div class="page-header">header</div>
-      <div class="page-body">body</div>
+      <div class="page-header">OPPORTUNITIES</div>
+      <div class="page-body">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
+        </p>
+        <ul>
+          <li>Text of the printing</li>
+          <li>Text of the printing</li>
+          <li>Text of the printing</li>
+          <li>Text of the printing</li>
+          <li>Text of the printing</li>
+          <li>Text of the printing</li>
+          <li>Text of the printing</li>
+        </ul>
+      </div>
     </div>
     <div class="page item-6">
-      <div class="page-header">header</div>
-      <div class="page-body">body</div>
+      <div class="page-header">OPPORTUNITIES</div>
+      <div class="page-body">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
     </div>
     <div class="page item-7">
-      <div class="page-header">header</div>
-      <div class="page-body">body</div>
+      <div class="page-header">OUR HISTORY-2010</div>
+      <div class="page-body">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
     </div>
     <div class="page item-8">
-      <div class="page-header">header</div>
-      <div class="page-body">body</div>
+      <div class="page-header">OUR HISTORY-2019</div>
+      <div class="page-body">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -43,7 +121,7 @@ export default {
 
 <style lang="scss" scoped>
 .page {
-  background: rgb(200, 200, 200);
+  background: #ededed;
   min-height: 20vh;
   border-radius: 1rem;
   padding: 1rem;
@@ -54,7 +132,8 @@ export default {
     border-bottom: 0.5px solid rgba($color: #333, $alpha: 0.5);
   }
   &-body {
-      padding: 1rem 0;
+    padding: 1rem 0;
+   
   }
 }
 .box {
@@ -63,12 +142,6 @@ export default {
   grid-template-columns: 1fr 1fr;
   column-gap: 10px;
   row-gap: 10px;
-  & .item-1 {
-  }
-  & .item-2 {
-  }
-  & .item-3 {
-  }
   & .item-4 {
     grid-row-start: 2;
     grid-row-end: 4;
@@ -81,5 +154,8 @@ export default {
     grid-column-start: 2;
     grid-column-end: 3;
   }
+}
+p::first-letter{
+  font-size: 25px;
 }
 </style>
--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,12 +1,52 @@
 <template>
   <div class="box">
     <div class="contact-form">
+      <form class="contact-form-aside">
+        <div>
+          <label for="userName">姓名</label>
+          <input
+            id="userName"
+            class="contact-form-control"
+            placeholder="輸入姓名"
+          />
+        </div>
+        <div>
+          <label for="userAddress">住址</label>
+          <input
+            id="userAddress"
+            class="contact-form-control"
+            placeholder="輸入住址"
+          />
+        </div>
+        <div>
+          <label for="userPhone">連絡電話</label>
+          <input
+            id="userPjone"
+            class="contact-form-control"
+            placeholder="輸入連絡電話"
+          />
+        </div>
+        <div>
+          <label for="userMail">E-mail</label>
+          <input
+            id="userMail"
+            class="contact-form-control"
+            placeholder="輸入電子郵件"
+          />
+        </div>
+      </form>
       <form>
-        <input />
+        <label for="userName">留言</label>
+        <textarea
+          id="userName"
+          class="contact-form-textarea"
+          placeholder="輸入留言"
+        />
+        <button class="contact-btn">POST COMMIT</button>
       </form>
     </div>
     <div class="contact-map">
-      <l-map :zoom="zoom" :center="center" style="height: 100%">
+      <l-map :zoom="zoom" :center="center" style="min-height: 100%">
         <l-tile-layer :url="url" :attribution="attribution" />
       </l-map>
     </div>
@@ -36,7 +76,47 @@ export default {
   column-gap: 10px;
 }
 .contact-form {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 10px;
+  &-aside {
+    display: grid;
+    grid-template-rows: 1fr 1fr 1fr;
+  }
+  &-control {
+    width: 100%;
+    padding: 0.375rem 0.75rem;
+    font-size: 1rem;
+    line-height: 1.5;
+    color: #495057;
+    background-color: #fff;
+    background-clip: padding-box;
+    border: 1px solid #ced4da;
+    border-radius: 0.25rem;
+    transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+  }
 }
-.contact-map {
+.contact-form-textarea {
+  width: 100%;
+  min-height: 80%;
+  color: #495057;
+  background-color: #fff;
+  background-clip: padding-box;
+  border: 1px solid #ced4da;
+  border-radius: 0.25rem;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+}
+.contact-btn {
+  width: 100%;
+  min-height: 13.5%;
+  color: #fff;
+  font-size: 30px;
+  background: #1eaf71;
+  border: 1px solid #ced4da;
+  border-radius: 0.25rem;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+  &:hover {
+    cursor: pointer;
+  }
 }
 </style>
